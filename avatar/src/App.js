b13a3245732ef4c231e15a46c1dae2eda0c3fcca
Tiyasa9 <tiyasasarkar092003@gmail.com>
2024-07-12 13:01:58 +0530

    Basic avatar page added

---
+++ avatar/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import AvatarCreation from './components/AvatarCreation';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <AvatarCreation />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ avatar/src/components/AvatarCreation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './AvatarCreation.css';
+
+function AvatarCreation() {
+  return (
+    <div className="AvatarCreation">
+      <h2>Create your Avatar</h2>
+      {/* Placeholder for avatar creation functionality */}
+      <div className="avatar-container">
+        {/* Add your avatar creation tools here */}
+      </div>
+    </div>
+  );
+}
+
+export default AvatarCreation;
